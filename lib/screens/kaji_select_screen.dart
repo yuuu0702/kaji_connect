ac729ccc2288731e4f64ec156e9c5b284bb7390e
yudai <yudai.0702wa@gmail.com>
2023-08-05 23:33:57 +0900

    riverpodつかったチェックリスト作成

--- lib/screens/kaji_select_screen.dart
+++ lib/screens/kaji_select_screen.dart
@@ -3,26 +3,88 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'home_screen.dart';
 
+@immutable
+class Todo {
+  const Todo(
+      {required this.id, required this.description, required this.completed});
+
+  // イミュータブルなクラスのプロパティはすべて `final` にする必要があります。
+  final String id;
+  final String description;
+  final bool completed;
+
+  // Todo はイミュータブルであり、内容を直接変更できないためコピーを作る必要があります。
+  // これはオブジェクトの各プロパティの内容をコピーして新たな Todo を返すメソッドです。
+  Todo copyWith({String? id, String? description, bool? completed}) {
+    return Todo(
+      id: id ?? this.id,
+      description: description ?? this.description,
+      completed: completed ?? this.completed,
+    );
+  }
+}
+
+// StateNotifierProvider に渡すことになる StateNotifier クラスです。
+// このクラスではステートを `state` プロパティの外に公開しません。
+// つまり、ステートに関しては public なゲッターやプロパティは作らないということです。
+// public メソッドを通じて UI 側にステートの操作を許可します。
+class TodosNotifier extends StateNotifier<List<Todo>> {
+  // Todo リストを空のリストとして初期化します。
+  TodosNotifier()
+      : super([
+          Todo(description: '部屋掃除', id: '1', completed: false),
+          Todo(description: '洗濯', id: '2', completed: false),
+          Todo(description: '料理', id: '3', completed: false),
+        ]);
+
+  // Todo の追加
+  void addTodo(Todo todo) {
+    // ステート自体もイミュータブルなため、`state.add(todo)`
+    // のような操作はできません。
+    // 代わりに、既存 Todo と新規 Todo を含む新しいリストを作成します。
+    // Dart のスプレッド演算子を使うと便利ですよ!
+    state = [...state, todo];
+    // `notifyListeners` などのメソッドを呼ぶ必要はありません。
+    // `state =` により必要なときに UI側 に通知が届き、ウィジェットが更新されます。
+  }
+
+  // Todo の削除
+  void removeTodo(String todoId) {
+    // しつこいですが、ステートはイミュータブルです。
+    // そのため既存リストを変更するのではなく、新しくリストを作成する必要があります。
+    state = [
+      for (final todo in state)
+        if (todo.id != todoId) todo,
+    ];
+  }
+
+  // Todo の完了ステータスの変更
+  void toggle(String todoId) {
+    state = [
+      for (final todo in state)
+        // ID がマッチした Todo のみ、完了ステータスを変更します。
+        if (todo.id == todoId)
+          // またまたしつこいですが、ステートはイミュータブルなので
+          // Todo クラスに実装した `copyWith` メソッドを使用して
+          // Todo オブジェクトのコピーを作る必要があります。
+          todo.copyWith(completed: !todo.completed)
+        else
+          // ID が一致しない Todo は変更しません。
+          todo,
+    ];
+  }
+}
+
+final todosProvider = StateNotifierProvider<TodosNotifier, List<Todo>>((ref) {
+  return TodosNotifier();
+});
+
 class CheckboxListTileRiverpod extends ConsumerWidget {
   CheckboxListTileRiverpod({Key? key}) : super(key: key);
 
-  //家事の名前MOCK
-  final List<String> kajinameMock = ['部屋掃除', '洗濯', '料理', '皿洗い'];
-
-  final AutoDisposeStateProvider<List<bool>> _checkedListProvider =
-      StateProvider.autoDispose<List<bool>>((ref) {
-    return List.generate(4, (index) => false);
-  });
-
-  final ProviderListenable<dynamic> _changeNotifierProvider =
-      ChangeNotifierProvider((ref) {
-    return ChangeNotifier();
-  });
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final List<bool> checkedList = ref.watch(_checkedListProvider);
-    final changeNotifier = ref.watch(_changeNotifierProvider);
+    List<Todo> todos = ref.watch(todosProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text("家事をえらんでね")),
@@ -31,19 +93,17 @@ class CheckboxListTileRiverpod extends ConsumerWidget {
           //家事リストビュー
           Expanded(
             child: ListView.separated(
-              itemBuilder: (context, index) => CheckboxListTile(
-                title: Text(kajinameMock[index]),
-                value: checkedList[index],
-                onChanged: (bool? checkedValue) {
-                  ref.read(_checkedListProvider.notifier).state[index] =
-                      checkedValue!;
-                  changeNotifier.notifyListeners();
-                },
-              ),
-              separatorBuilder: (context, index) {
-                return const Divider(height: 0.5);
+              itemBuilder: (context, index) {
+                final todo = todos[index];
+                return CheckboxListTile(
+                  value: todo.completed,
+                  onChanged: (value) =>
+                      ref.read(todosProvider.notifier).toggle(todo.id),
+                  title: Text(todo.description),
+                );
               },
-              itemCount: kajinameMock.length,
+              separatorBuilder: (context, index) => const Divider(height: 0.5),
+              itemCount: todos.length,
             ),
           ),
           FilledButton(
